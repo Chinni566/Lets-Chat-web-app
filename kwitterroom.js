ff9cfa54b695606e9774e3d96087c7fba893239c
Neeraj <78613839+Chinni566@users.noreply.github.com>
2021-12-02 18:54:07 +0100

    Add files via upload

---
+++ kwitterroom.js
@@ -0,0 +1,32 @@
+
+
+var firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+
+    user_name = localStorage.getItem("Username");
+    document.getElementById("welcome_user_name").innerHTML = "Welcome "+user_name+"!";
+
+    function addroom() {
+           room_name = document.getElementById("room_name").value;
+  
+          localStorage.setItem("Roomname",room_name);
+      
+          window.location = "kwitterroom.html";
+    }
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+     Room_names = childKey;
+    //Start code
+
+    //End code
+    });});}
+getData();
